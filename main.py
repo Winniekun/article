d04e0c4648ef64ea94b7360904198cd0a1085123
weikunkun <kongwiki@163.com>
2023-02-28 21:26:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from github import Github
 from feedgen.feed import FeedGenerator
 from lxml.etree import CDATA
 
-MD_HEAD = """## Gitblog
-My personal blog using issues and GitHub Actions (随意转载，无需署名)
-[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
+MD_HEAD = """## Article
+Technical article using issues and GitHub Actions,
+[reference](https://github.com/yihong0618/gitblog)
 """
 
 BACKUP_DIR = "BACKUP"
